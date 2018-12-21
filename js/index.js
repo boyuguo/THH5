c2a5e5d02904ff773b55b291d0b48af0bb76770b
guoboyu <guoboyu@chicv.com>
2018-12-21 20:04:23 +0800

    update

--- js/index.js
+++ js/index.js
@@ -5,17 +5,47 @@ $(function(){
     $('.gender-module').removeClass('hide');
   })
 
-  var gender ;
+ //     性别        日历类型        时间        地区
+  var gender='',calendarType='',dataVal='',localVal='';
+  // 选择性别切换下一页
   $('.gender-list').on('click','.gender-item',function(){
     var selectVal = $(this).find('.inp').data('val');
     gender = selectVal;
-
-    console.log(gender,'this is gender');
-
     $('.container-box').find('.wrap-box').addClass('hide');
     $('.form-module').removeClass('hide');
   })
 
+  // 调时间插件
+  var calendardatetime = new lCalendar();
+			calendardatetime.init({
+				'trigger': '#dataVal',
+				'type': 'datetime'
+			});
+
+// 点击切换日历类型
+  $('.calendar-box').on('click','.btn',function(){
+    $(this).siblings().removeClass('btn-active');
+    $(this).addClass('btn-active');
+    var curCalendar = $(this).data('calendar-type');
+    calendarType = curCalendar;
+  })
+// 地址插件
+  var $target = $('#localVal');
+      $target.citySelect();
+      $target.on('click', function (event) {
+          event.stopPropagation();
+          $target.citySelect('open');
+      });
+      $target.on('done.ydui.cityselect', function (ret) {
+          $(this).val(ret.provance + ' ' + ret.city + ' ' + ret.area);
+      });
+
+  // 点击提交按钮
+  $('#submitBtn').on('click',function(){
+    var dataVal = $('#dataVal').val(),
+        localVal = $('#localVal').val();
+    console.log(gender,calendarType,dataVal,localVal,'this is val');
+  })
 
 
 })
